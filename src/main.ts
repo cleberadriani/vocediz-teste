fab1359c7fffff6aeb0d383d09ca6c1b7f0b90cd
Cleber Adriani <cleberadriani@gmail.com>
2022-12-08 18:26:06 -0300

    mvp1

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 8080;
+  console.log(`serviço iniciado na porta:${port}`);
+
+  await app.listen(port);
+}
+bootstrap();
