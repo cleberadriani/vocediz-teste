76de4daf53696d73e134b0fa165296c26f9bf837
Cleber Adriani <cleberadriani@gmail.com>
2022-12-09 19:37:35 -0300

    change port to 80

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 80;
   console.log(`serviço iniciado na porta:${port}`);
 
   await app.listen(port);
